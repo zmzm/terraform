3e6e05b41c853a30b57e10853034d3fd5aedb355
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2021-11-24 01:17:17 +0300

    add test output to terraform

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -19,3 +19,8 @@ resource "aws_s3_bucket" "terraform_test_bucket" {
     enabled = true
   }
 }
+
+# After resource creation, you can output any valuable information into the console.
+output "terraform_test_bucket_remote_id" {
+  value = aws_s3_bucket.terraform_test_bucket.id
+}
