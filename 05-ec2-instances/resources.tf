4d66dd701b75c36c0c2b0284405a79d70b916236
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2021-12-06 23:51:39 +0300

    create server security group

---
+++ 05-ec2-instances/resources.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "http_server_sg" {
+  name        = "http_server_sg"
+  description = "Allow HTTP/SSH inbound/outbound traffic"
+  vpc_id      = "vpc-0dd32c134eec70aa4"
+
+  # HTTP traffic
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SSH
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
